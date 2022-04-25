dfea5a51375351843fbbb768580d1f84bb0fbe75
Juan J. Lorenzo Sáez <juan-lorenzo@hotmail.es>
2022-04-25 20:09:30 +0200

    Installed angular material

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatCardModule
+  ],
+  exports: [
+    CommonModule,
+    MatCardModule
+  ]
+})
+export class SharedModule { }
